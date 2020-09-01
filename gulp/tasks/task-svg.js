05b3f2fdc4e3ac254b8bf817234764fadb76f788
Jan Andrle <andrle.jan@centrum.cz>
2020-09-01 12:27:25 +0200

    :hammer:

--- gulp/tasks/task-svg.js
+++ gulp/tasks/task-svg.js
@@ -1,6 +1,6 @@
 /* jshint node: true */
 module.exports= function({app, gulp, error, $g, $o, $run}){
-    /* jshint -W061 */const gulp_place= $g.place({ variable_eval: (str)=> eval(str) });/* jshint +W061 */
+    /* jshint -W061 */const gulp_place= $g.place({ variable_eval: (str)=> eval(str), filesCleaner: content=> content.replace(/ xmlns="http:\/\/www.w3.org\/2000\/svg"/g, "") });/* jshint +W061 */
     const { src, bin }= app.directories;
     return function(cb){
         gulp.src([src+"*.svg", '!'+src+'*.sub.svg'])
