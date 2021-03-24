a079715716af01c5d8f03428e1db64605680b423
Jan Andrle <andrle.jan@centrum.cz>
2021-03-24 16:58:01 +0100

    :tada: skrývání afra

--- gulp/tasks/task-svg.js
+++ gulp/tasks/task-svg.js
@@ -11,7 +11,7 @@ module.exports= function({app, gulp, error, $g, $o, $run}){
     /* jshint +W061 */
     const { src, bin }= app.directories;
     return function(cb){
-        gulp.src([src+"*.svg", '!'+src+'*.sub.svg'])
+        gulp.src([ src+"*.svg", '!'+src+'*.sub.svg' ])
             .pipe(gulp_place({ folder: src, string_wrapper: '' }))
             .pipe(gulp.dest(bin))
             .on('end', cb);
