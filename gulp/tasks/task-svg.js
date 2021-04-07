b0ec2bb1b21bfc77760ea0df0a8d607907e40961
Jan Andrle <andrle.jan@centrum.cz>
2021-04-07 14:01:16 +0200

    :boom: automatic ID based on filename

--- gulp/tasks/task-svg.js
+++ gulp/tasks/task-svg.js
@@ -11,9 +11,22 @@ module.exports= function({app, gulp, error, $g, $o, $run}){
     /* jshint +W061 */
     const { src, bin }= app.directories;
     return function(cb){
-        gulp.src([ src+"*.svg", '!'+src+'*.sub.svg' ])
-            .pipe(gulp_place({ folder: src, string_wrapper: '' }))
-            .pipe(gulp.dest(bin))
-            .on('end', cb);
+        gulp.src([ src+"**/*.sub.svg" ])
+            .pipe($g.replace(/<svg (id="[^"]*")?/, function(){
+                return `<svg id="${getID(this.file)}"`;
+            }))
+            .pipe(gulp.dest(src))
+            .on('end', function(){
+                gulp.src([ src+"*.svg", '!'+src+'*.sub.svg' ])
+                    .pipe(gulp_place({ folder: src, string_wrapper: '' }))
+                    .pipe(gulp.dest(bin))
+                    .on('end', cb);
+            });
     };
+    function getID({ basename, dirname }){
+        const part= dirname.split("bigheads-parts\\")[1];
+        const name= basename.split(".")[0];
+        if(!part) return name;
+        return part+"-"+name;
+    }
 };
